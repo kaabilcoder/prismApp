7ab020685125ab0f63ab6a1516a24ad26bb46c2b
kaabilcoder <kaabilcoder@gmail.com>
2025-04-07 16:46:07 +0530

    added routing - Frontend finished

--- recapbrain-frontend/src/App.tsx
+++ recapbrain-frontend/src/App.tsx
@@ -1,13 +1,19 @@
 import Dashboard from './pages/Dashboard'
 import './App.css'
 import { Signup } from './pages/Signup'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signin } from './pages/Signin'
 
 
 const App = () => {
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
