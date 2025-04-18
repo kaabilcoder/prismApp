fc06e50e79e8877ad1341f2b709948ba18a544b2
kaabilcoder <kaabilcoder@gmail.com>
2025-04-18 17:32:43 +0530

    Clean up old recapbrain-frontend files and finalize renaming to prism-frontend

--- prism-frontend/src/components/SideBar.tsx
+++ prism-frontend/src/components/SideBar.tsx
@@ -7,13 +7,13 @@ export function SideBar() {
     return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 border-gray-200 pl-4 pt-6 text-gray-600">
         <div className="text-3xl pb-3">
             <div className="flex gap-2">
-                <Logo/>
-                <span>Recape Brain</span>
+                <Logo />
+                <span>Prism</span>
             </div>
         </div>
         <div className="mt-6">
-        <SideBarContent text="Twitter" icon={<TwitterIcon />} />
-        <SideBarContent text="Youtube" icon={<YoutubeIcon />} />
+            <SideBarContent text="Twitter" icon={<TwitterIcon />} />
+            <SideBarContent text="Youtube" icon={<YoutubeIcon />} />
         </div>
     </div>
 }
