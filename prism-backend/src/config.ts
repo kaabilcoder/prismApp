3d7f9bb9a37a9eb3aaca556eb047c7f398a76ae3
kaabilcoder <kaabilcoder@gmail.com>
2025-10-15 00:06:21 +0530

    created docker image of the frontend application

--- prism-backend/src/config.ts
+++ prism-backend/src/config.ts
@@ -1,2 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config()
+
+const ENV = process.env.NODE_ENV || "development";
+
+let FRONTEND_URI: string;
+
+if(ENV=='development'){
+    FRONTEND_URI = "http://localhost:5173";
+} else if (ENV=='docker'){
+    FRONTEND_URI = 'http://localhost';
+}
+
+export { FRONTEND_URI };
 export const JWT_PASSWORD = "!23123";
-export const FRONTEND_URI = "http://localhost:5173"
+
+
